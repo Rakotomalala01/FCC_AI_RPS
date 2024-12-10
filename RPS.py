877b42d6d7293f99b1432ab7a7772d4a695876a8
Rakotomalala01 <johary.rakotmoalala2001@gmail.com>
2024-12-10 00:58:02 -0500

    JR: Kris detected

--- RPS.py
+++ RPS.py
@@ -13,66 +13,46 @@ def player(prev_play, opponent_history=[],
                 "RR": 0, "RP": 0, "RS": 0, "PR": 0, "PP": 0, "PS": 0, "SR": 0, "SP": 0, "SS": 0
             }], 
             my_history=[], 
-            counter_history = [], player_type = [PlayerType.UNKNOWN]
+            counter_history = [], 
+            player_type = [PlayerType.UNKNOWN]
            ):
 
     if not prev_play:
         prev_play = 'R'
     
     ## DETECTION PHASE 
-    # First move will be R to allow kris detection
-    if len(opponent_history) == 0 :
- 
-        move = 'R'
-        my_history.append(move)
-        # Hard code the first value for Kris
-        counter_history.append('R')
-        opponent_history.append(prev_play)
-        return 'R'
-    
     # Training phase to detect the type of the opponent player
-    elif len(opponent_history) < 300:
+    if len(opponent_history) < 300:
         move = random.choice(['R', 'P', 'S'])
         my_history.append(move)
         counter_history.append(counter(move))
-        opponent_history.append(prev_play)
-
-        # print(player_type)
-        # print('my_history')
-        # print(my_history[0:12])
-
-        # print('counter')
-        # print(counter_history[0:10])
-        # print('opponent')
-        # print(opponent_history[0:10])
-        return move
 
     # Predict the opponent type 
     elif len(opponent_history) == 300:
-        print(my_history[0])
         if is_quincy(opponent_history):
-            player_type = PlayerType.QUINCY
-        elif is_kris(counter_history, opponent_history):
-            player_type = PlayerType.KRIS
+            print('QUINCY ZA')
+            player_type[0] = PlayerType.QUINCY
+        elif is_Kris(counter_history, opponent_history, my_history):
+            print('KRIIIIS')
+            player_type[0] = PlayerType.KRIS
+        else:
+            print('UNKNOWW')
 
-        opponent_history.append(prev_play)
-
-        print(player_type)
-        print('my_history')
-        print(my_history[0:12])
-        print('counter')
-        print(counter_history[0:10])
-        print('opponent')
-        print(opponent_history[0:10])
-        
-        
-        return counter(det_nxt_move(player_type, opponent_history))
+        move = 'R'
 
     # Counter depending on the opponent type 
     else:
-        return 'R'
-           
+        move = 'R'
+    opponent_history.append(prev_play)
 
+    if(len(opponent_history) == 1000):
+        opponent_history.clear()
+        my_history.clear()
+        counter_history.clear()
+        player_type[0] = PlayerType.UNKNOWN
+
+    return move
+        
 
 
 
@@ -85,28 +65,10 @@ def is_quincy(opponent_history):
     # Compare the first 10 elements of opponent history with double cycle
     return opponent_history[:10] == double_cycle
 
+def is_Kris(counter_history, opponent_history, my_history):
+    # - 2 to handle the offset between the array
 
-
-def is_kris(counter_history, opponent_history):
-
-
-    return counter_history == opponent_history
-
-
-def det_quincy_nxt_move(opponent_history):
-    quincy_cycle = ["R", "R", "P", "P", "S"]
-    current_position = len(opponent_history)% len(quincy_cycle)
-    return quincy_cycle[current_position]
-
-def det_nxt_move(playerType, opponent_history):
-      match playerType:
-        case PlayerType.QUINCY:
-            return det_quincy_nxt_move( opponent_history)
-        case PlayerType.KRIS:
-            return 'S'
-        case _:
-            return 'R'
-          
+    return counter_history[0: len(counter_history) -2] == opponent_history[2:]
 
 def counter(move):
     match move:
